1ad26b6982598d4caabfac8417c69585c8b0daf2
Ivan Jovic <100125296+IvanJovicTN@users.noreply.github.com>
2022-08-31 15:12:13 +0200

    Add files via upload

---
+++ scripts/project/main.js
@@ -0,0 +1,36 @@
+
+// Import any other script files here, e.g.:
+// import * as myModule from "./mymodule.js";
+
+var  data= '';
+// var  textInstance = '';
+
+runOnStartup(async runtime =>
+{
+	// Code to run on the loading screen.
+	// Note layouts, objects etc. are not yet available.
+	
+	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
+	
+	window.addEventListener("message", (event) => {
+		data = event.data;
+		console.log("iFrame recieved: " + data);
+		//textInstance.text = data;
+	}, false);
+	}); 
+
+async function OnBeforeProjectStart(runtime)
+{
+	// Code to run just before 'On start of layout' on
+	// the first layout. Loading has finished and initial
+	// instances are created and available to use here.
+	
+	runtime.addEventListener("tick", () => Tick(runtime));
+	
+	//textInstance = runtime.objects.Text();
+}
+
+function Tick(runtime)
+{
+	// Code to run every tick
+}
---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,29 @@
+var postMsg = ''
+
+function postText(value){
+	console.log('iFrame sender: ' + value) 
+	parent.postMessage(value, "*")
+}
+
+
+const scriptsInEvents = {
+
+		async Gameset_Event3_Act7(runtime, localVars)
+		{
+			postText(runtime.globalVars.FinalScore)
+		},
+
+		async Gameset_Event4_Act5(runtime, localVars)
+		{
+			postText(runtime.globalVars.FinalScore)
+		},
+
+		async Gameset_Event13_Act4(runtime, localVars)
+		{
+			postText(runtime.globalVars.FinalScore)
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
