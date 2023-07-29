1e15f559756e4f47699f6788b3952d8c82a0745f
Matheus N <matheusnaschaves@gmail.com>
2023-07-29 11:00:23 -0300

    Revert "ws"
    This reverts commit d0d99858769fbca54d9521e7158cc09b12472f3a.

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -8,61 +8,18 @@ function postText(value){
 
 const scriptsInEvents = {
 
-	async Gameset_Event1_Act2(runtime, localVars)
-	{
-		runtime.globalVars.webSocket.onMessage = (event) => {
-			if (event.data.startsWith('s:')){
-				const score_ = event.data.split(':')[1];
-				console.log(score_);
-				runtime.globalVars.score = score_;
-				runtime.callFunction("updateScore")
-			}
-		}
-		
-	},
-
 	async Gameset_Event11_Act7(runtime, localVars)
 	{
-		runtime.globalVars.webSocket.close();
+		postText(runtime.globalVars.postScore)
 	},
 
 	async Gameset_Event14_Act4(runtime, localVars)
 	{
-		runtime.globalVars.webSocket.close();
-	},
-
-	async Gameset_Event15_Act3(runtime, localVars)
-	{
-		runtime.globalVars.webSocket.send("s:25")
-	},
-
-	async Gameset_Event21_Act3(runtime, localVars)
-	{
-		runtime.globalVars.webSocket.send("s:500")
+		postText(runtime.globalVars.postScore)
 	},
 
 	async Gameset_Event22_Act2(runtime, localVars)
 	{
-		
-	},
-
-	async Addset_Event27_Act1(runtime, localVars)
-	{
-		const queryParams = new URLSearchParams(window.location.search)
-		const token = queryParams.get('token');
-		const gameId = queryParams.get('gameId');
-		
-		try{
-			const webSocket = new WebSocket('wss://arcade.stage.legacyarcade.com/ws', [token,gameId]);
-			runtime.globalVars.webSocket = webSocket;
-			webSocket.onopen = (event) =>{
-				runtime.callFunction('startendless');
-			};
-		}catch(e){
-			const textInstance = runtime.objects.ErrorText.getFirstInstance()
-			textInstance.text = "ERROR CONNECTING"
-			console.log("error connecting to server", e)
-		}
 		
 	}
 
