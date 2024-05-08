7fe87151986cbfffe2b92d7caa291ba3ada17ac8
tanjounokamioku <matheusnaschaves@gmail.com>
2024-05-08 11:41:03 -0300

    time scale adaptation

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -54,51 +54,6 @@ const scriptsInEvents = {
 	async Gameset_Event21_Act2(runtime, localVars)
 	{
 		
-	},
-
-	async Addset_Event27_Act1(runtime, localVars)
-	{
-		const queryParams = new URLSearchParams(window.location.search)
-		const token = queryParams.get('token');
-		const gameId = queryParams.get('gameId');
-		console.log("clicked")
-					const textInstance = runtime.objects.ErrorText.getFirstInstance()
-					textInstance.text = "PLEASE WAIT..."
-					const button = runtime.objects.BtnPlay.getFirstInstance();
-					button.destroy(); 
-		// Add a variable to track if the WebSocket is already connected or in the process of connecting
-		let isWebSocketConnectingOrConnected = false; 
-					
-		try {
-		    // Check if the WebSocket is already connected or in the process of connecting
-		    if (!isWebSocketConnectingOrConnected) {
-		        const webSocket = new WebSocket('wss://arcade.shadow.legacyarcade.com/ws', [token, gameId]);
-		        runtime.globalVars.webSocket = webSocket;
-		
-		        webSocket.onopen = (event) => {
-		            isWebSocketConnectingOrConnected = true;  // Set the flag to true when connection is established
-		            runtime.callFunction('startendless');
-		        };
-		
-		        webSocket.onclose = (event) => {
-		            isWebSocketConnectingOrConnected = false; // Reset the flag when the connection is closed
-		        };
-		
-		        webSocket.onerror = (event) => {
-		            isWebSocketConnectingOrConnected = false; // Reset the flag on error
-		        };
-		
-		        runtime.globalVars.playable = 1;
-		    }
-		} catch (e) {
-		    const textInstance = runtime.objects.ErrorText.getFirstInstance();
-		    textInstance.text = "ERROR CONNECTING";
-		    console.log("error connecting to server", e);
-		}
-		
-		
-		
-		
 	}
 
 };
